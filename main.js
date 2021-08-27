c31274a88a08aabba9d8cc2bccf0e2568f04f573
rittvik-arrvind <70020673+rittvik-arrvind@users.noreply.github.com>
2021-08-27 16:39:41 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+
+let btn = document.getElementById("getTimezone");
+
+
+if(btn !== null){
+btn.addEventListener("click",Timezone);
+}
+
+function Timezone(){
+let time = document.getElementById("showTimezone");
+time.innerHTML = Date();
+}
